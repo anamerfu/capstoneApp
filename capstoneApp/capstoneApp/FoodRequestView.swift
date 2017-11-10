d8dccadfdda15826d320650d0f024c0f9aed79e3
anamerfu <anamerfu@gmail.com>
2017-11-09 23:23:27 -0500

    fixed lost changed from merge in foodRequestView - adding setNewProperty() and addNewRequest() func

--- capstoneApp/capstoneApp/FoodRequestView.swift
+++ capstoneApp/capstoneApp/FoodRequestView.swift
@@ -36,20 +36,25 @@ class FoodRequestView: UIView {
         
         print("requested \(numberOfFoodsRequested) \(foods[randomFoodNumber])")
 
-        
-        //currentRequestNumber = Int(numberOfFoods)
-        
+    
+       
+    }
+    func addNewRequest () {
         for index in 1...numberOfFoodsRequested{
- 
+            
             let foodImageView: UIImageView = UIImageView(image: foodImage)
             foodImageView.frame = CGRect(x: Int(5 + (index * 25)), y: 5, width: 25, height: 25)
             self.addSubview(foodImageView)
             
         }
-        
-       
     }
-    
+    func setNewProperty() {
+        self.randomFoodNumber = Int (arc4random_uniform ( UInt32(foods.count) ) )
+        self.randomFoodName = foods[randomFoodNumber] + ".png"
+        self.foodImage = UIImage(named: randomFoodName)
+        self.numberOfFoodsRequested = Int(arc4random_uniform(4) + 1)
+        self.currentRequest = foods[randomFoodNumber]
+    }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
