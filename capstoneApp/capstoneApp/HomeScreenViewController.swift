6104b243cb53af8604445b59791d570430f11048
larabee90 <lauradouglas90@gmail.com>
2017-12-12 15:55:52 -0500

    Added instructions and fixed alignment on HomeScreenViewController.

--- capstoneApp/capstoneApp/HomeScreenViewController.swift
+++ capstoneApp/capstoneApp/HomeScreenViewController.swift
@@ -15,9 +15,9 @@ class HomeScreenViewController: UIViewController {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
         
-        print("hello i am home screen")
+        let button = UIButton(frame: CGRect(x: (self.view.frame.size.width - 100) / 2, y: self.view.frame.size.height - 150, width: 100, height: 50))
+        let label = UILabel(frame: CGRect(x: (self.view.frame.size.width - 400) / 2, y: (self.view.frame.size.height - 600) / 2, width: 400, height: 500))
         
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
         button.backgroundColor = UIColor(hex: "00dfdc")
         button.layer.cornerRadius = 10
         button.setTitle("Play", for: .normal)
@@ -25,7 +25,15 @@ class HomeScreenViewController: UIViewController {
         button.setTitleColor(UIColor.white, for: .normal)
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         
+//        label.center = CGPoint(x: self.view.frame.size.width - 325, y: self.view.frame.size.height - 250)
+        label.textAlignment = .center
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.font = UIFont(name: "Nunito-Bold", size: 20)
+        label.text = "Look for the patch of grass and tap to find the bunny! Help him find the food he wants to eat by looking around the room and tapping on the correct food!"
+        
         self.view.addSubview(button)
+        self.view.addSubview(label)
         
     }
     
